43484cc46154457c9d2670b46d7ae1043138db66
ScriptCamilo <script.camilo@gmail.com>
2022-09-02 17:22:48 -0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.2c8cecd21315f11967fc2e4c9315f1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8426b8e3992724aba998ba13beb28c5",
+    "url": "/trybe-movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "0654f285c1148f8e933c",
+    "url": "/trybe-movie-cards-library-crud/static/css/main.b18869a8.chunk.css"
+  },
+  {
+    "revision": "1b93ae96655bed7919d2",
+    "url": "/trybe-movie-cards-library-crud/static/js/2.5a8b1fb4.chunk.js"
+  },
+  {
+    "revision": "0654f285c1148f8e933c",
+    "url": "/trybe-movie-cards-library-crud/static/js/main.a6418a5b.chunk.js"
+  },
+  {
+    "revision": "a54f9351318a350604f3",
+    "url": "/trybe-movie-cards-library-crud/static/js/runtime-main.862c5f09.js"
+  }
+]);
